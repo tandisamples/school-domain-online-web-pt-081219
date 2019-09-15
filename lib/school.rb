4b07e6412dadb68e95a512d1b233d6cc33ec6b62
Tandi Samples <tandisamples@gmail.com>
2019-09-15 17:50:30 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
+class School
+
+  def initialize(name)
+    @name = name
+    @roster= {}
+  end
+
+  def roster
+    return @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      if !(@roster[grade].include?(name))
+        @roster[grade] << name
+      end
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    return roster[grade]
+  end
+
+  def sort
+    newHash = {}
+    roster.each{|grade, student|
+      newHash[grade] = student.sort
+    }
+    newHash
+  end 
+
+end
+
 # code here!
